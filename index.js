bb3dcde5928faf0a87062c42d371012a44b1a4ed
Jerkbert <raballantyne@gmail.com>
2019-09-03 21:56:58 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
 // Add your code here
+
+function submitData(name, email) {
+    
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+            name, 
+            email
+        })
+    }
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(object) {
+            document.body.innerHTML = object["id"]
+        })
+        .catch(function(error) {
+            
+            document.body.innerHTML = error.message
+        })
+}
+
+
+
+
+
+
+
+
+
+
+
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+// }
+
+// let configObj = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+// }
+
+// let dogURL = "http://localhost:3000/dogs"
+
+// fetch(dogURL, configObj)
+//     .then(function(response) {
+//         return response.json()
+//     })
+//     .then(function(object) {
+//         console.log(object)
+//     })
+//     .catch(function(error) {
+//         alert("uh oh")
+//         console.log(error.message)
+//     })
